196995ec7d9354dca53d636ed2a0581b155d696e
z11t <i.zikurat@gmail.com>
2024-07-07 20:07:26 +0400

    test v0.0.23

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-function tap() {
+document.getElementById('tap-tap').addEventListener('click', function() {
     var p = document.getElementById('amount-number');
     var value = parseInt(p.textContent, 10);
     p.textContent = value + 1;
-};
+});
